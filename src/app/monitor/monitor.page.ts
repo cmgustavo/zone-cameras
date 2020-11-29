fc86285f97558499789aa8153470aeb4ca4f38d1
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2020-11-29 00:17:56 -0300

    Remove console.log

--- src/app/monitor/monitor.page.ts
+++ src/app/monitor/monitor.page.ts
@@ -9,18 +9,15 @@ import { ZoneminderService } from '../services/zoneminder.service';
   templateUrl: './monitor.page.html',
   styleUrls: ['./monitor.page.scss']
 })
-export class MonitorPage implements OnInit {
+export class MonitorPage {
   public streamUrl: string;
   constructor(
     private route: ActivatedRoute,
     private zmService: ZoneminderService
   ) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
     const id = this.route.snapshot.paramMap.get('id');
-    console.log('[monitor.page.ts:15]', id); /* TODO */
-
     this.streamUrl = this.zmService.viewStream(id);
-    console.log('[monitor.page.ts:20]', this.streamUrl); /* TODO */
   }
 }
